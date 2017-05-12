674adb2e22d722dfa542bbfe2c9686ad4e63f3cb
Preston Matheson <pmphoto123@gmail.com>
2017-05-12 11:08:59 -0600

    added the initial files and HTTParty gem

---
+++ lib/ta_api.rb
@@ -0,0 +1,11 @@
+class Ta_Api
+  def ta_patch(issue_id, resolution)
+    response = HTTParty.patch("https://devcamp.com/hand_raises/#{issue_id}/?hand_raise[#{resolution}]")
+    puts response
+  end
+
+  def ta_post(text)
+    options = { :body => {:status => text}, :basic_auth => @auth }
+    self.class.post('/statuses/update.json', options)
+  end
+end
