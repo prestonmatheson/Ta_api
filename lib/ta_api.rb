76857bf87611939cea1251c04398671317ef0552
Preston Matheson <pmphoto123@gmail.com>
2017-05-12 12:26:23 -0600

    changed the response

--- lib/ta_api.rb
+++ lib/ta_api.rb
@@ -1,7 +1,7 @@
 class Ta_Api
   def ta_patch(issue_id, resolution)
     response = HTTParty.patch("https://devcamp.com/hand_raises/#{issue_id}/?hand_raise[#{resolution}]")
-    puts response
+    puts response.body
   end
 
   def ta_post(text)
