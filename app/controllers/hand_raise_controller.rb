fc35f8a9c32e8086c124913eb2e71d226d86195d
Trevor <trevorhoward2@hotmail.com>
2017-05-12 13:04:11 -0600

    created hand raise controller

---
+++ app/controllers/hand_raise_controller.rb
@@ -0,0 +1,8 @@
+class HandRaiseController < ApplicationController
+	def create
+	end
+
+	def update
+	end
+	
+end
--- app/models/hand_raise.rb
+++ app/models/hand_raise.rb
@@ -1,2 +1,5 @@
 class HandRaise < ApplicationRecord
+	validates_presence_of :hand_raise_id, :question, :email, :name, :slack_username
+	include Response
+  include ExceptionHandler
 end
